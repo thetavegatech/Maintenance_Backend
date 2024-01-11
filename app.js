f4ad2e26ebd024f2169bd8e9dbe54c559963c30d
thetavegatech <thetavegatech@gmail.com>
2024-01-11 12:21:11 +0530

    commit2

--- app.js
+++ app.js
@@ -73,19 +73,6 @@ app.get('/getBreakdownData', async (req, res) => {
     }
   });
 
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, 'attachments/');
-//   },
-//   filename: (req, file, cb) => {
-//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-//     cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.').pop());
-//   },
-// });
-
-// const upload = multer({ storage: storage });
-
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
             var dirName =path.join(process.cwd(), './files/')
